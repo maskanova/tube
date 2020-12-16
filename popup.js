6d7e156c0eeb265b73bcdae56d64beaa3ee7289e
maskanova <69957650+maskanova@users.noreply.github.com>
2020-12-16 22:30:24 +0700

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,11 @@
+// Hide Popup
+function HidePopup() {
+document.getElementById('popup').style.display = 'none';
+window.open("https://trends.hasdukmerahputih.com")
+}
+// Open Popup
+function OpenPopup(){
+setInterval(function(){ 
+document.getElementById('popup').style.display = 'block';
+}, 60*1000);
+}
